e9167198827f0bf5688cc862c7f8edad72c69cae
Shea Kusiak <shkusiak@gmail.com>
2018-02-11 20:21:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,13 +5,14 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index.map do |name, index|
-      array = []
-      array.push("#{index+1}. #{name}")
-      puts "The line is currently: #{array.join}"
-    end
 
-#    puts "The line is currently: #{array.join}"
+    #katz_deli.each_with_index.map do |name, index|
+    #  array = []
+    #  array.push("#{index+1}. #{name}")
+    #  puts "The line is currently: #{array.join}"
+    #end
+
+    puts "The line is currently: #{katz_deli.join}"
   end
 end
 
