6e871b514ebe3dcdb222d2e0764f2e74f228354e
Shea Kusiak <shkusiak@gmail.com>
2018-02-11 18:49:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli}"
+    puts "The line is currently: #{katz_deli.join}"
   end
 end
 
