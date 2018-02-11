400c16c1a42560a03dddd4c3569cf25b799b17b8
Shea Kusiak <shkusiak@gmail.com>
2018-02-11 20:30:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,10 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-
+    new_arr = katz_deli.map.with_index {|name, index|
+      "#{index.to_i + 1}. #{name}"
+    }
+    puts new_arr
     #katz_deli.each_with_index.map do |name, index|
     #  array = []
     #  array.push("#{index+1}. #{name}")
