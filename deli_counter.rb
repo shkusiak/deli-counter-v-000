441a776f1ba5d9004f3267d072877fbd59081b8e
Shea Kusiak <shkusiak@gmail.com>
2018-02-11 20:43:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length = 0
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "currently serving #{katz_deli[0]}."
